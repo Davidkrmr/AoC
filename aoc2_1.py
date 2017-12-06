f08502a4f6f3fae9cc86aa29a66c32cb5507ae6a
David Kramer <davkramer@gmail.com>
2017-12-06 22:37:51 +0100

    Bored. Polished.

--- aoc2_1.py
+++ aoc2_1.py
@@ -2,14 +2,10 @@ import unittest
 
 
 def create_table(data):
-    table = []
-    for line in data.split('\n'):
-        l = map(int, line.split())
-        table.append(l)
-    return table
+    return [map(int, line.split()) for line in data.split('\n')]
 
 
-def calculate_diff(l):
+def calc_diff(l):
     return max(l) - min(l)
 
 
@@ -17,10 +13,7 @@ def calc(data):
     result = 0
     table = create_table(data)
 
-    for t in table:
-        result += calculate_diff(t)
-
-    return result
+    return sum((calc_diff(t) for t in table))
 
 
 class Test(unittest.TestCase):
@@ -31,7 +24,7 @@ class Test(unittest.TestCase):
 
     def test_calculate_diff(self):
         table = [5, 1, 9, 5]
-        self.assertEqual(calculate_diff(table), 8)
+        self.assertEqual(calc_diff(table), 8)
 
     def test_calculate(self):
         self.assertEqual(calc("5 1 9 5\n7 5 3\n2 4 6 8"), 18)
--- aoc2_2.py
+++ aoc2_2.py
@@ -2,39 +2,39 @@ import unittest
 
 
 def create_table(data):
-    table = []
-    for line in data.split('\n'):
-        l = map(int, line.split())
-        table.append(l)
-    return table
+    return [map(int, line.split()) for line in data.split('\n')]
 
 
 def calc_div(l):
+    result = 0
     for first in l:
         for second in l:
             if first % second == 0 and first != second:
-                return first / second
+                result = first / second
+                break
+
+    return result
 
 
 def calc(data):
     result = 0
     table = create_table(data)
 
-    for t in table:
-        result += calc_div(t)
-
-    return result
+    return sum((calc_div(t) for t in table))
 
 
 class Test(unittest.TestCase):
 
     def test_create_table(self):
         table = [[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]
-        self.assertEqual(create_table("5 1 9 5\n7 5 3\n2 4 6 8"), table)
+        self.assertEqual(create_table("5 1 9 5 \n 7 5 3 \n 2 4 6 8"), table)
 
     def test_calc_div(self):
         table = [5, 9, 2, 8]
         self.assertEqual(calc_div(table), 4)
+
+    def test_calc(self):
+        self.assertEqual(calc("5 1 9 5 \n7 5 3 \n2 4 6 8"), 9)
 
 
 if __name__ == '__main__':
