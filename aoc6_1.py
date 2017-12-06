5aea62c99d9cfa25d4f048cfe82ea40d1b103387
David Kramer <davkramer@gmail.com>
2017-12-06 07:30:37 +0100

    Solve part one day 6

--- aoc6_1.py
+++ aoc6_1.py
@@ -1,9 +1,56 @@
 import unittest
 
+def redistribute(l):
+    sel = max(l)
+    sel_index = l.index(sel)
+    l[sel_index] = 0
+
+    while sel > 0:
+        sel_index += 1
+        l[sel_index % len(l)] = l[sel_index % len(l)] + 1
+        sel -= 1
+    
+    return list(l)
+
+
+def run(l):
+    prev_state = []
+    redistributed = list(l)
+    cycles = 0
+
+    while redistributed not in prev_state:
+        print redistributed
+        prev_state.append(list(redistributed))
+        redistributed = redistribute(redistributed)
+        cycles += 1
+
+    return cycles
+
+def prep(s):
+    return [int(s) for s in s.split('\t')]
+
 class Test(unittest.TestCase):
     
-    def test(self):
-        self.assertEquals(1, 2)
+    def test_redistribute(self):
+        data = [0, 2, 7, 0]
+        self.assertEquals(redistribute(data), [2, 4, 1, 2])
+
+    def test_redistribute_iter(self):
+        data = [0, 2, 7, 0] 
+        result = redistribute(data)
+        self.assertEquals(result, [2, 4, 1, 2])
+        result = redistribute(result)
+        self.assertEquals(result, [3, 1, 2, 3])
+        result = redistribute(result)
+        self.assertEquals(result, [0, 2, 3, 4])
+        result = redistribute(result)
+        self.assertEquals(result, [1, 3, 4, 1])
+        result = redistribute(result)
+        self.assertEquals(result, [2, 4, 1, 2])
+
+    def test_run(self):
+        data = [0, 2, 7, 0]
+        self.assertEquals(run(data), 5)
 
 
 if __name__ == '__main__':
